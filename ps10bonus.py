e90a396e1a7afa8372f221072884fb34daa73bf8
dcxSt <dcxstephen@live.co.uk>
2021-01-03 17:08:38 +0100

    quiver plot

--- ps10bonus.py
+++ ps10bonus.py
@@ -4,6 +4,7 @@ We approximate the loop with a bunch of line segments, then add up all the vecto
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 sin = math.sin
 cos = math.cos
@@ -20,10 +21,10 @@ def polar_to_cartesian(r,theta,phi):
 	return r*sin(theta)*cos(phi) , r*sin(theta)*sin(phi) , r*cos(theta) # x,y,z
 
 def circle_loop(r=1,n=50): # returns circle loop in x,y plane
-	theta = np.linspace(0,2*PI,n) 
+	theta = np.linspace(0,2*PI,n+1) 
 	loopx = [r*cos(t) for t in theta] + [r*cos(theta[0])]
 	loopy = [r*sin(t) for t in theta] + [r*sin(theta[0])]
-	loopz = [0 for i in range(n+1)]
+	loopz = [0 for i in range(n+2)]
 	return loopx,loopy,loopz 
 
 def long_line_segment(z=100):
@@ -60,8 +61,12 @@ def get_vec_pot_at(loopx,loopy,loopz,x,y,z): # returns A, the vector potential a
 		# print("p1,p2,x,y,z\t{},{},{},{},{}".format(p1,p2,x,y,z)) # trace
 		zhat = (p2-p1) / norm(p2-p1)
 		yvec = np.array([x,y,z]) - p1
-		yvec = yvec - yvec * dot(yvec,zhat) / norm(yvec)
-		yhat = yvec / norm(yvec)
+		try:
+			yvec = yvec - yvec * dot(yvec,zhat) / norm(yvec)
+			yhat = yvec / norm(yvec) 
+		except:
+			print("problem!!, exception caught in vec_pot_line_seg")
+			yhat = np.array([0.0,1.0,0.0]) # temporary
 		xhat = crossp(yhat,zhat)
 		
 		# find the vector potential from equation in question 2, it's in the zhat direction
@@ -71,7 +76,12 @@ def get_vec_pot_at(loopx,loopy,loopz,x,y,z): # returns A, the vector potential a
 		p2xyz = p2 - np.array([x,y,z])
 		x2,y2,z2 = dot(p2xyz,xhat),dot(p2xyz,yhat),dot(p2xyz,zhat) # if all went well x1 and x2 should be 0
 		# print("x1 and x2 should  e zero : x1:{},x2:{}".format(x1,x2)) # trace
-		a_seg = ln(z2 + sqrt(z2**2 + y2**2)) - ln(z1 + sqrt(z1**2 + y1**2))
+		try:
+			a_seg = ln(z2 + sqrt(z2**2 + y2**2)) - ln(z1 + sqrt(z1**2 + y1**2))
+		except:
+			print("error encountered, printing\nz2,z1 should be positive:\n{}\n{}".format(z2,z1))
+			print("set a_seg to zero")
+			a_seg = 0.0
 		return a_seg * zhat
 		# returns 3 vector
 
@@ -84,7 +94,7 @@ def get_vec_pot_at(loopx,loopy,loopz,x,y,z): # returns A, the vector potential a
 
 
 
-def get_b(loop,x,y,z,d=0.01):
+def get_b(loop,x,y,z,d=0.001):
 	loopx,loopy,loopz = loop()
 	a000 = get_vec_pot_at(loopx,loopy,loopz,x,y,z)
 	a100 = get_vec_pot_at(loopx,loopy,loopz,x+d,y,z)
@@ -93,7 +103,12 @@ def get_b(loop,x,y,z,d=0.01):
 	bx = (a010[2] - a000[2])/d - (a001[1] - a000[1])/d
 	by = (a001[0] - a000[0])/d - (a100[2] - a100[2])/d
 	bz = (a100[1] - a000[1])/d - (a010[0] - a000[0])/d
-	return np.array([bx,by,bz])
+	
+	cutoff = 7
+	if norm([bx,by,bz]) < cutoff: # if it's not too big
+		return np.array([bx,by,bz])
+	return np.zeros(3)
+
 
 def plot_loop(loopx,loopy,loopz):
 	fig = plt.figure()
@@ -103,6 +118,37 @@ def plot_loop(loopx,loopy,loopz):
 	plt.show()
 	return	
 
+def calculate_potential_field(loopx,loopy,loopz,xarr,yarr,zarr):
+	# loopx,loopy,loopz should be same shape
+	# xarr, yarr, zarr should be same shape
+	# returns uarr,varr,warr - the quiver plot xyz, same shape as (x|y|z)arr (tanjent vectors)
+	shape = xarr.shape
+	if yarr.shape!=shape or zarr.shape!=shape:raise Exception("The shapes of grid are not the same! incompatible!")
+	if len(loopx)!= len(loopy) or len(loopy)!= len(loopz):raise Exception("Loops not good.")
+	uarr,varr,warr = np.zeros(shape),np.zeros(shape),np.zeros(shape); 
+	for i in range(shape[0]):
+		for j in range(shape[1]):
+			for k in range(shape[2]):
+				uvw = get_vec_pot_at(loopx,loopy,loopz,xarr[i,j,k],yarr[i,j,k],zarr[i,j,k])
+				uarr[i,j,k],varr[i,j,k],warr[i,j,k] = uvw[0],uvw[1],uvw[2]
+	return uarr,varr,warr
+
+def calculate_magnetic_field(loop,xarr,yarr,zarr):
+	shape = xarr.shape
+	uarr,varr,warr = np.zeros(shape),np.zeros(shape),np.zeros(shape); 
+	for i in range(shape[0]):
+		for j in range(shape[1]):
+			for k in range(shape[2]):
+				uvw = get_b(loop,xarr[i,j,k],yarr[i,j,k],zarr[i,j,k])
+				uarr[i,j,k],varr[i,j,k],warr[i,j,k] = uvw[0],uvw[1],uvw[2]
+	return uarr,varr,warr
+	
+def quiver_plot(xarr,yarr,zarr,uarr,varr,warr):
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
+	ax.quiver(xarr,yarr,zarr,uarr,varr,warr,length=0.2,normalize=True)
+	plt.show()
+	return
 
 if __name__ == "__main__":
 #	xarr,yarr,zarr = make_cube_grid(s=2)
@@ -121,17 +167,52 @@ if __name__ == "__main__":
 	loopname = input("enter loopname: ")
 	loop = configurations[loopname]
 	loopx,loopy,loopz = loop()
+	choice = 0
+	while choice not in [1,2]:
+		choice = int(input("do you want quiver plots (2) or get a specific field (1)?: ")) 
+	if choice ==1:
+		print("\nNow enter the specific coordinates you want me to evaluate the vector potential and magnetic field at?")
+		x = float(input("enter x coord: "))
+		y = float(input("enter y coord: "))
+		z = float(input("enter z coord: "))
 
-	print("\nNow enter the coordinates you want me to evaluate the vector potential at:")
-	x = float(input("enter x coord: "))
-	y = float(input("enter y coord: "))
-	z = float(input("enter z coord: "))
-
-	vector_potential = get_vec_pot_at(loopx,loopy,loopz,x,y,z)
+		vector_potential = get_vec_pot_at(loopx,loopy,loopz,x,y,z)
 	
-	print("\nThe vector potential at {},{},{} is \n{}".format(x,y,z,vector_potential))	
-	b = get_b(loop,x,y,z)
-	print("\nThe magnetic field at {},{},{} is\n{}".format(x,y,z,b))
+		print("\nThe vector potential at {},{},{} is \n{}".format(x,y,z,vector_potential))	
+		b = get_b(loop,x,y,z)
+		print("\nThe magnetic field at {},{},{} is\n{}".format(x,y,z,b))
+	
+	elif choice==2:
+		# quiver plot magnetic field
+		s,d = 0.7,0.2
+		xarr,yarr,zarr = make_cube_grid(s=s,d=d)
+		uarr,varr,warr = calculate_magnetic_field(loop,xarr,yarr,zarr)
+		fig = plt.figure()
+		ax = fig.gca(projection='3d')
+		#o = np.sqrt(uarr**2 + varr**2 + zarr**2)
+		#o = np.random.random(uarr.shape)
+		c = np.hypot(np.hypot(uarr,varr),warr)
+		#c = np.concatenate((c,np.repeat(c,2)))
+		#c=plt.cm.hsv(c)
+		q=ax.quiver(xarr,yarr,zarr,uarr,varr,warr,color='g',cmap='Greens',alpha=0.9,length=0.05,lw=2,label="B field")
+		#q.set_array(np.random.rand(np.prod(xarr.shape)))
+		q.set_array(c.flatten())
+		
+		#ax2 = fig.gca(projection='3d')
+		## magnetic potential plot
+		#loopx,loopy,loopz = loop()
+		#uarr,varr,warr = calculate_potential_field(loopx,loopy,loopz,xarr,yarr,zarr)
+		#r=ax2.quiver(xarr,yarr,zarr,uarr,varr,warr,color='r',cmap='Reds',alpha=0.8,length=0.05,lw=2,label="A field")
+		#c = np.hypot(np.hypot(uarr,varr),warr)
+		#r.set_array(c.flatten)
+
+		if loop.__name__ == "long_line_segment":
+			plt.plot([0,0],[0,0],[-s,s],color='b',label="loop")
+		else: 
+			plt.plot(loopx,loopy,loopz,color='b',label="loop")
+		plt.legend()
+		plt.show()
+
 
 #	s,d=2,0.4
 #	xarr,yarr,zarr = np.meshgrid(np.arange(-s,s,d),
